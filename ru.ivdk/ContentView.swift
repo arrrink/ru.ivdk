79418f1f72b4317869382ba8c86382619d437e80
arrrink <arina.nefa@gmail.com>
2020-12-11 21:20:33 +0400

    Initial Commit

---
+++ ru.ivdk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ru.ivdk
+//
+//  Created by Арина Нефёдова on 11.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ru.ivdk/ru_ivdkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ru_ivdkApp.swift
+//  ru.ivdk
+//
+//  Created by Арина Нефёдова on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ru_ivdkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
